23358cc3b74c462b50684e795719cdc389bd4726
NoahStoryM <noahstorym@gmail.com>
2026-03-28 13:16:30 +0800

    Improve the type of `wait/fc`

--- context/main.rkt
+++ context/main.rkt
@@ -10,9 +10,25 @@
   [absurd (∀ (a) (→ ⊥ a))]
   [label (→* () (Prompt-TagTop) Label)]
   [goto (∀ (a) (case→ (→ Label ⊥) (→ (¬ a) a ⊥)))]
-  [current-continuation (∀ (a) (case→ (→* () (Prompt-TagTop) (∪ a (¬ a))) (→ (¬ a) a ⊥)))]
-  [wait-for-future-continuation (∀ (a ...) (→* ((→ (→ a ... a ⊥) (Values a ... a))) (Prompt-TagTop) (¬ (→ a ... a ⊥))))]
-  [return-with-current-continuation (∀ (a ...) (→* ((→ (Values a ... a))) (Prompt-TagTop) (¬ (→ a ... a ⊥))))]
+  [current-continuation
+   (∀ (a)
+      (case→
+       (→* () (Prompt-TagTop) (∪ a (¬ a)))
+       (→ (¬ a) a ⊥)))]
+  [wait-for-future-continuation
+   (∀ (a b c ...)
+      (case→
+       (→* ((→ (→ ⊥) (Values)))
+           (Prompt-TagTop)
+           (¬ (→ ⊥)))
+       (→* ((→ (→ a c ... c ⊥) (Values b c ... c)))
+           (Prompt-TagTop)
+           (¬ (→ (∪ a b) c ... c ⊥)))))]
+  [return-with-current-continuation
+   (∀ (a ...)
+      (→* ((→ (Values a ... a)))
+          (Prompt-TagTop)
+          (¬ (→ a ... a ⊥))))]
   [return-with-values (∀ (a) (→ a (¬ (¬ a))))])
 (provide (rename-out [current-continuation cc]
                      [wait-for-future-continuation wait/fc]
